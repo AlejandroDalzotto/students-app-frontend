1ea7141b7a12772dfb5e6ee404910be9f7d0cfa5
AlejandroDalzotto <aledalzotto15@gmail.com>
2023-12-08 18:00:14 -0300

    fix incorrect app name

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,1 +1,1 @@
-export const APP_NAME = "Vanado"
+export const APP_NAME = "Varano"
--- app/ui/TitleNav.tsx
+++ app/ui/TitleNav.tsx
@@ -2,6 +2,6 @@ import { APP_NAME } from "../lib/constants";
 
 export default function TitleNav() {
   return (
-    <h1 className="text-5xl font-bold leading-tight tracking-tight">Bienvenido a {APP_NAME}</h1>
+    <h1 className="text-5xl font-bold leading-tight tracking-tight">Bienvenido a <span className="underline hover:text-blue-500 select-none">{APP_NAME}</span></h1>
   )
 }
