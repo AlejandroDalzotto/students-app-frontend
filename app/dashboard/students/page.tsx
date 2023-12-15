73950cdfa35fdb8f22c7f3477f1411c1ad93d5dd
AlejandroDalzotto <aledalzotto15@gmail.com>
2023-12-15 20:42:52 -0300

    move students from component to page

--- app/dashboard/students/page.tsx
+++ app/dashboard/students/page.tsx
@@ -1,5 +1,9 @@
-import StudentsTable from "@/app/ui/dashboard/StudentsTable";
 import Header from "@/app/ui/dashboard/Header";
+import SearchBar from "@/app/ui/dashboard/SearchBar";
+import { Suspense } from "react";
+import { randomUUID } from "crypto";
+import ListOfStudentsSkeleton from "@/app/ui/skeletons/ListOfStudentsSkeleton";
+import Table from "@/app/ui/dashboard/Table";
 
 export default function StudentsPage({
   searchParams
@@ -15,7 +19,24 @@ export default function StudentsPage({
     <section className="w-full relative rounded-lg">
       <Header />
       <article className="w-full flex">
-        <StudentsTable query={query} />
+        <div className="relative flex flex-col items-center flex-grow self-stretch">
+          <div className="flex justify-between py-8 items-center self-stretch">
+            <SearchBar />
+            {/* Action buttons go here */}
+          </div>
+
+          {/* Students table */}
+          <section className="flex items-center gap-5 flex-grow self-stretch">
+            {/* Left panel */}
+            <article className="flex min-w-[64rem] flex-col justify-center items-start gap-5 flex-grow self-stretch max-w-5xl">
+              <Suspense key={randomUUID() + query} fallback={<ListOfStudentsSkeleton />}>
+                <Table query={query} />
+              </Suspense>
+            </article>
+
+            {/* Right panel go here */}
+          </section>
+        </div>
       </article>
     </section>
   )
