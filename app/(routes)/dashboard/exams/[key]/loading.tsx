dbad657a8cc52d3838cc9e7259df6aa833bf9e02
AlejandroDalzotto <aledalzotto15@gmail.com>
2024-02-29 22:21:55 -0300

    Implement load page to single exams

---
+++ app/(routes)/dashboard/exams/[key]/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="bg-black w-full relative h-full row-[span_2/span_-1] p-6 rounded-lg flex flex-col">
+      <p>cargando</p>
+    </div>
+  )
+}
