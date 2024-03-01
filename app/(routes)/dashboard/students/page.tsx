1ac5ecd71018733baa356a379a7f012fff15ceae
AlejandroDalzotto <aledalzotto15@gmail.com>
2024-03-01 20:27:35 -0300

    add fixed table height with grid

--- app/(routes)/dashboard/students/page.tsx
+++ app/(routes)/dashboard/students/page.tsx
@@ -35,34 +35,32 @@ export default async function StudentsPage({
 
   return (
     <section className="w-full relative h-full row-[span_2/span_-1] p-6 flex flex-col">
-      <div className="relative">
-        <header className="flex w-full justify-between py-6 items-center">
-          <SearchBar placeholder="Ingresa nombre y/o apellido" label="Buscar alumno..." />
-          <div className="flex items-center gap-x-5">
-            <EditButtom active={Boolean(dniStudent)} nagivateTo={`/dashboard/students/edit?sid=${dniStudent}`} />
-            <DeleteButton />
-            <AddButton nagivateTo="/dashboard/students/add" title="Agregar un nuevo alumno al sistema" />
-          </div>
-        </header>
+      <header className="flex w-full justify-between py-6 items-center">
+        <SearchBar placeholder="Ingresa nombre y/o apellido" label="Buscar alumno..." />
+        <div className="flex items-center gap-x-5">
+          <EditButtom active={Boolean(dniStudent)} nagivateTo={`/dashboard/students/edit?sid=${dniStudent}`} />
+          <DeleteButton />
+          <AddButton nagivateTo="/dashboard/students/add" title="Agregar un nuevo alumno al sistema" />
+        </div>
+      </header>
 
-        <section className="grid grid-cols-12 grid-flow-row h-full w-full relative gap-x-4">
+      <section className="grid grid-cols-12 grid-flow-row h-full w-full relative gap-x-4">
 
-          <article className="col-span-9 grid">
-            <Suspense key={query} fallback={<ListOfStudentsSkeleton />}>
-              <Table query={query} currentPage={currentPage} />
-            </Suspense>
-
-            <div className="mt-5 flex w-full justify-center">
-              <Pagination totalPages={totalPages} />
-            </div>
-          </article>
-
-          <Suspense key={query + dniStudent} fallback={<DataPanelSkeleton />}>
-            <StudentDataPanel dni={dniStudent} />
+        <article className="col-span-9 grid grid-rows-[80%_minmax(0,20%)]">
+          <Suspense key={query} fallback={<ListOfStudentsSkeleton />}>
+            <Table query={query} currentPage={currentPage} />
           </Suspense>
 
-        </section>
-      </div>
+          <div className="mt-5 flex w-full justify-center">
+            <Pagination totalPages={totalPages} />
+          </div>
+        </article>
+
+        <Suspense key={query + dniStudent} fallback={<DataPanelSkeleton />}>
+          <StudentDataPanel dni={dniStudent} />
+        </Suspense>
+
+      </section>
     </section>
   )
 }
