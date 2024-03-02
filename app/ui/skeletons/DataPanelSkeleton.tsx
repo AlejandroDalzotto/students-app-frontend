20f2593691d8c552ed80d17d294fafcf95d52365
AlejandroDalzotto <aledalzotto15@gmail.com>
2024-03-02 02:01:51 -0300

    add white color in dark mode to skeleton

--- app/ui/skeletons/DataPanelSkeleton.tsx
+++ app/ui/skeletons/DataPanelSkeleton.tsx
@@ -1,40 +1,41 @@
 export default function DataPanelSkeleton() {
+
+  // relative rounded-lg col-span-3 flex flex-col items-center gap-y-5 p-5 bg-black/5 dark:bg-black/20
   return (
-    <div className="relative rounded-lg col-span-3 flex flex-col items-center gap-y-5 p-5 h-full bg-black/5 dark:bg-black/20">
+    <div className="relative rounded-lg col-span-3 flex flex-col items-center gap-y-5 p-5 h-full bg-black/5 dark:bg-white/5">
 
       <div className="flex w-full items-center justify-between">
-        <div className="h-[120px] aspect-square rounded-full bg-black/5"></div>
-        <div className="h-9 w-40 rounded-lg bg-black/5"></div>
-        <div className="h-9 w-40 rounded-lg bg-black/5"></div>
+        <div className="h-[120px] aspect-square rounded-full bg-black/5 dark:bg-white/5 animate-pulse"></div>
+        <div className="h-9 w-full ml-4 rounded-lg bg-black/5 dark:bg-white/5 animate-pulse"></div>
       </div>
 
-      <div className="h-6 mt-5 w-full rounded-lg bg-black/5"></div>
+      <div className="h-6 mt-5 w-full rounded-lg bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '100ms' }}></div>
 
       <div className="flex w-full gap-x-10 items-center justify-between">
-        <div className="h-9 w-full rounded-lg bg-black/5"></div>
-        <div className="h-9 w-full rounded-lg bg-black/5"></div>
+        <div className="h-9 w-full rounded-lg bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '200ms' }}></div>
+        <div className="h-9 w-full rounded-lg bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '200ms' }}></div>
       </div>
 
-      <div className="h-6 mt-5 w-full rounded-lg bg-black/5"></div>
+      <div className="h-6 mt-5 w-full rounded-lg bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '300ms' }}></div>
 
       <div className="flex w-full gap-x-10 items-center justify-between">
-        <div className="h-9 w-full rounded-lg bg-black/5"></div>
-        <div className="h-9 w-full rounded-lg bg-black/5"></div>
+        <div className="h-9 w-full rounded-lg bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '400ms' }}></div>
+        <div className="h-9 w-full rounded-lg bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '400ms' }}></div>
       </div>
 
-      <div className="h-6 mt-5 w-full rounded-lg bg-black/5"></div>
+      <div className="h-6 mt-5 w-full rounded-lg bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '500ms' }}></div>
 
       <div className="flex w-full gap-x-10 items-center justify-between">
-        <div className="h-9 w-full rounded-lg bg-black/5"></div>
-        <div className="h-9 w-full rounded-lg bg-black/5"></div>
+        <div className="h-9 w-full rounded-lg bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '600ms' }}></div>
+        <div className="h-9 w-full rounded-lg bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '600ms' }}></div>
       </div>
       <div className="flex w-full gap-x-10 items-center justify-between">
-        <div className="h-9 w-full rounded-lg bg-black/5"></div>
-        <div className="h-9 w-full rounded-lg bg-black/5"></div>
+        <div className="h-9 w-full rounded-lg bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '700ms' }}></div>
+        <div className="h-9 w-full rounded-lg bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '700ms' }}></div>
       </div>
       <div className="flex w-full gap-x-10 items-center justify-between">
-        <div className="h-9 w-full rounded-lg bg-black/5"></div>
-        <div className="h-9 w-full rounded-lg bg-black/5"></div>
+        <div className="h-9 w-full rounded-lg bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '800ms' }}></div>
+        <div className="h-9 w-full rounded-lg bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '800ms' }}></div>
       </div>
     </div>
   )
--- app/ui/skeletons/ListOfStudentsSkeleton.tsx
+++ app/ui/skeletons/ListOfStudentsSkeleton.tsx
@@ -1,50 +1,60 @@
 export default function ListOfStudentsSkeleton() {
   return (
-    <div className="w-full row-span-3 flex flex-col gap-y-2 p-2 relative rounded-lg">
+    <div className="w-full flex flex-col gap-y-2 relative">
 
       {/* Table heads */}
-      <div className="w-full">
-        <div className="h-10 relative flex gap-x-10">
-          <div className="h-full w-10 flex items-center">
-            <span className="h-2 block w-full rounded-full bg-black/5 animate-pulse"></span>
-          </div>
-          <div className="h-full w-20 flex items-center">
-            <span className="h-2 block w-full rounded-full bg-black/5 animate-pulse" style={{ animationDelay: '150ms' }}></span>
-          </div>
-          <div className="h-full w-14 flex items-center">
-            <span className="h-2 block w-full rounded-full bg-black/5 animate-pulse" style={{ animationDelay: '300ms' }}></span>
-          </div>
-          <div className="h-full w-10 flex items-center">
-            <span className="h-2 block w-full rounded-full bg-black/5 animate-pulse" style={{ animationDelay: '450ms' }}></span>
-          </div>
-          <div className="h-full w-20 flex items-center">
-            <span className="h-2 block w-full rounded-full bg-black/5 animate-pulse" style={{ animationDelay: '600ms' }}></span>
-          </div>
+      <div className="h-14 py-4 px-12 relative flex gap-x-10 bg-black/5 dark:bg-white/5">
+        <div className="h-full w-10 flex items-center">
+          <span className="h-4 block w-full rounded-full bg-black/5 dark:bg-white/5 animate-pulse"></span>
         </div>
+        <div className="h-full w-20 flex items-center">
+          <span className="h-4 block w-full rounded-full bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '150ms' }}></span>
+        </div>
+        <div className="h-full w-14 flex items-center">
+          <span className="h-4 block w-full rounded-full bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '300ms' }}></span>
+        </div>
+        <div className="h-full w-10 flex items-center">
+          <span className="h-4 block w-full rounded-full bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '450ms' }}></span>
+        </div>
+        <div className="h-full w-20 flex items-center">
+          <span className="h-4 block w-full rounded-full bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '600ms' }}></span>
+        </div>
+        <div className="h-full w-20 flex items-center">
+          <span className="h-4 block w-full rounded-full bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '600ms' }}></span>
+        </div>
+        <div className="h-full w-10 flex items-center">
+          <span className="h-4 block w-full rounded-full bg-black/5 dark:bg-white/5 animate-pulse"></span>
+        </div>
+      </div>
 
-        {/* Table rows */}
-        <div className="flex flex-col gap-y-5">
-          <div>
-            <div className="w-full h-11 rounded bg-black/5 animate-pulse"></div>
-          </div>
-          <div>
-            <div className="w-full h-11 rounded bg-black/5 animate-pulse" style={{ animationDelay: '100ms' }}></div>
-          </div>
-          <div>
-            <div className="w-full h-11 rounded bg-black/5 animate-pulse" style={{ animationDelay: '200ms' }}></div>
-          </div>
-          <div>
-            <div className="w-full block h-11 rounded bg-black/5 animate-pulse" style={{ animationDelay: '300ms' }}></div>
-          </div>
-          <div>
-            <div className="w-full block h-11 rounded bg-black/5 animate-pulse" style={{ animationDelay: '400ms' }}></div>
-          </div>
-          <div>
-            <div className="w-full block h-11 rounded bg-black/5 animate-pulse" style={{ animationDelay: '500ms' }}></div>
-          </div>
-          <div>
-            <div className="w-full block h-11 rounded bg-black/5 animate-pulse" style={{ animationDelay: '600ms' }}></div>
-          </div>
+      {/* Table rows */}
+      <div className="flex flex-col gap-y-2">
+        <div>
+          <div className="w-full h-11 rounded bg-black/5 dark:bg-white/5 animate-pulse"></div>
+        </div>
+        <div>
+          <div className="w-full h-11 rounded bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '100ms' }}></div>
+        </div>
+        <div>
+          <div className="w-full h-11 rounded bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '200ms' }}></div>
+        </div>
+        <div>
+          <div className="w-full block h-11 rounded bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '300ms' }}></div>
+        </div>
+        <div>
+          <div className="w-full block h-11 rounded bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '400ms' }}></div>
+        </div>
+        <div>
+          <div className="w-full block h-11 rounded bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '500ms' }}></div>
+        </div>
+        <div>
+          <div className="w-full block h-11 rounded bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '600ms' }}></div>
+        </div>
+        <div>
+          <div className="w-full block h-11 rounded bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '700ms' }}></div>
+        </div>
+        <div>
+          <div className="w-full block h-11 rounded bg-black/5 dark:bg-white/5 animate-pulse" style={{ animationDelay: '800ms' }}></div>
         </div>
       </div>
     </div>
