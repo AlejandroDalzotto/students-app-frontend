45c4411aedbb73c948a1b96d154b4ee897fb76f7
AlejandroDalzotto <aledalzotto15@gmail.com>
2024-03-04 22:09:27 -0300

    refactor schemas to single file

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,28 +1,3 @@
-import * as z from "zod";
 
-export const LoginSchema = z.object({
-  username: z.string().min(1, {
-    message: "El nombre de usuario es requerido."
-  }),
-  password: z.string().min(1, {
-    message: "La contraseña es requerida.",
-  }),
-});
 
-export const RegisterSchema = z.object({
-  name: z.string().min(3, {
-    message: "El nombre es requerido.",
-  }),
-  lastname: z.string().min(1, {
-    message: "El apellido es requerido.",
-  }),
-  username: z.string().min(1, {
-    message: "El nombre de usuario es requerido.",
-  }),
-  email: z.string().min(1, { message: "El email es requerido" }).email({
-    message: "Formato de email invalido. Ej: example@gmail.com",
-  }),
-  password: z.string().min(6, {
-    message: "Mínimo seis (6) caracteres son requeridos.",
-  }),
-});
+export * from './auth.schemas'
