37c2b7b2aeba8080a545aa4c7c5e9f0f4af8014e
AlejandroDalzotto <aledalzotto15@gmail.com>
2024-03-04 22:08:03 -0300

    refactor base_url

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,9 +1,12 @@
 export const APP_NAME = "Varano";
 
-export const BASE_AUTH_URL = "http://localhost:8080/auth";
-export const BASE_STUDENT_URL = "http://localhost:8080/api/student";
-export const BASE_COURSE_URL = "http://localhost:8080/api/course"
-export const BASE_EXAMS_URL = "http://localhost:8080/api/exams"
-export const BASE_SUBJECT_URL = "http://localhost:8080/api/subject"
+const BASE_URL = "http://localhost:8080"
+
+export const BASE_AUTH_URL = `${BASE_URL}/auth`;
+export const BASE_STUDENT_URL = `${BASE_URL}/api/student`;
+export const BASE_COURSE_URL = `${BASE_URL}/api/course`
+export const BASE_EXAMS_URL = `${BASE_URL}/api/exams`
+export const BASE_SUBJECT_URL = `${BASE_URL}/api/subject`
+export const BASE_MODULE_URL = `${BASE_URL}/api/module`
 
 export const ITEMS_PER_PAGE = 10;
