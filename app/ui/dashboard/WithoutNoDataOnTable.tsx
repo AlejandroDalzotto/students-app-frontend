1d1af2e4a559ea71c609c18677808072865146b7
AlejandroDalzotto <aledalzotto15@gmail.com>
2024-03-09 20:19:38 -0300

    add default view when there aren't data to render on the tables

---
+++ app/ui/dashboard/WithoutNoDataOnTable.tsx
@@ -0,0 +1,7 @@
+export default function WithoutNoDataOnTable() {
+  return (
+    <div className="rounded-lg col-span-full bg-black/5 w-full dark:bg-black/20 grid place-content-center">
+      <span className="text-3xl dark:text-white/60 font-bold italic">No hay datos para mostrar</span>
+    </div>
+  )
+}
